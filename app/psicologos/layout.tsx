456fde0cd32a72470bc4dc44a5c135a339fb3056
Straforini <134074601+PauloStraforini@users.noreply.github.com>
2025-05-13 09:15:56 -0300

    feat: wrap children in QueryProvider for context in RootLayout

--- app/psicologos/layout.tsx
+++ app/psicologos/layout.tsx
@@ -1,17 +1,11 @@
 // app/layout.tsx
 'use client'
 
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from '@/lib/queryClient'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-br">
-      <body>
-        <QueryClientProvider client={queryClient}>
+        <div>
           {children}
-        </QueryClientProvider>
-      </body>
-    </html>
+        </div>
   )
 }
